0ae3dffbb6b8beb4ab73e114ae9adc84221dc8be
Max Latoche <max.latoche@gmail.com>
2022-09-25 20:54:20 -0400

    sum_to_n compiles (argument === 3)

--- sum_to_n.asm
+++ sum_to_n.asm
@@ -1,30 +1,29 @@
 section .text
 global sum_to_n
 sum_to_n:
-    push    %rbp                ;
-    mov     %rbp, %rsp
-    mov     -0x4(%rbp), %edi    ; add argument to stack frame
-    mov     -0x8(%rbp), $0x0    ; assign 0 to total
-    mov     -0xc(%rbp), $0x0    ; assign 0 to i
+    push    rbp
+    mov     rbp, rsp
+    mov     [rbp-4], edi    ; add argument to stack frame
+    mov     byte[rbp-8], 0x0    ; assign 0 to total
+    mov     byte[rbp-12], 0x0    ; assign 0 to i
     jmp     loop
 
 loop:
-    mov     %eax, -0xc(%rbp)    ; move i to eax reg
-    cmp     %eax, -0x4(%rbp)    ; compare n to i
-    jlte    increment
-
+    mov     eax, -0xc    ; move i to eax reg
+    cmp     eax, -0x4    ; compare n to i
+    jle     increment
+    ret
 
 increment:
-    add     -0x8(%rbp), %eax    ; add i to total
-    add     %eax, $0x1          ; add 1 to i
-
+    add     [rbp-8], eax    ; add i to total
+    add     eax, 0x1          ; add 1 to i
 
 _main:
-    push    %rbp
-    mov     %rbp, %rsp
-    sub     %rsp, $0x10
-    mov     %edi, $0x3
+    push    rbp
+    mov     rbp, rsp
+    sub     rsp, 0x10
+    mov     edi, 0x3
     call    sum_to_n
-    pop     %rbp
+    pop     rbp
 	ret
 
